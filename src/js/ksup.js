2125abdbb33c17baa19628b4aca2e2eb38d6bcba
DSStepanov <DSStepanov@lanit.ru>
2019-06-13 15:58:50 +0500

    Начало работы + верстка табов

---
+++ src/js/ksup.js
@@ -0,0 +1,20 @@
+$(function() {
+
+	// Табы
+	$('.tabs__nav-item').click(function(e) {
+		e.preventDefault();
+
+		$(this)
+			.addClass('tabs__nav-item--active')
+			.siblings()
+			.removeClass('tabs__nav-item--active')
+
+		var destination = $(this).find('.tabs__link').attr('href');
+		
+		$('.tabs__tab').fadeOut(0);
+
+		$('.tabs__tab#' + destination).fadeIn(400);
+	});
+	
+
+});
