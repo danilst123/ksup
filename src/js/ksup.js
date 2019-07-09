1e1d0f1564849a45a26bd9310bc86e71950f16b8
DSStepanov <DSStepanov@lanit.ru>
2019-07-09 16:29:51 +0500

    Доверстка модалок + правки таблиц

--- src/js/ksup.js
+++ src/js/ksup.js
@@ -15,6 +15,48 @@ $(function() {
 
 		$('.tabs__tab' + destination).fadeIn(400);
 	});
+
+
+	// Закрыть модальное окно
+	$('.modal__close, .modal__bg').click(function() {
+		$(this).closest('.modal').fadeOut(400);
+	});
+
+
+	// Расчет максимальной высоты тела и максимальной ширины контейнера модального окна
+	function setModalSizes() {
+
+		var windowWidth = $(window).width(),
+				windowHeight = $(window).outerHeight(),
+				modalHeadHeight = $('.modal__head').outerHeight(),
+				modalBodyHeight = $('.modal__body').outerHeight(),
+				modalBoxMaxWidth = parseInt($('.modal__box').css('max-width'));
+
+		var modalBodyMaxHeight;
+
+		modalBodyMaxHeight = windowHeight - modalHeadHeight - 40;
+
+		// если на десктопах появляется скролл, который влияет на ширину контента
+		if ( modalBodyHeight > modalBodyMaxHeight ) {
+			modalBoxMaxWidth = 1260;
+		}
+
+		$('.modal__body').css({
+			'max-height': modalBodyMaxHeight + 'px'
+		});
+
+		$('.modal__box').css({
+			'max-width': modalBoxMaxWidth + 'px'
+		});
+	}
+
+
+	setModalSizes(); // Расчет максимальной высоты тела и максимальной ширины контейнера модального окна
+
+
+	$(window).resize(function() {
+		setModalSizes(); // Расчет максимальной высоты тела и максимальной ширины контейнера модального окна
+	});
 	
 
 });
